63c7d370dd5f233d64d59c4d5fc88de91e7aec89
Cesardrom <cesardguezr20@gmail.com>
2024-10-14 22:40:37 +0100

    Subimos el proyecto de dor terminado

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,75 +1,87 @@
 import Champion from './champions.js';
-import songs from '../sources/assests/songs/*.mp3'
-import Player from './player.js';
 
 var Champions = [];
 
-
 const button = document.querySelector("button");
+const audio = document.getElementById("background_music");
+
 button.addEventListener("click", () => {
+    if (audio.readyState >= 2) {
+        audio.play().catch(error => {
+            console.error("Error al intentar reproducir el audio:", error);
+        });
+    } else {
+        audio.addEventListener('canplaythrough', () => {
+            audio.play().catch(error => {
+                console.error("Error al intentar reproducir el audio:", error);
+            });
+        });
+    }
     document.querySelector('#button').style.visibility = 'hidden';
     document.querySelector('#champion_list').style.visibility = 'visible';
     startChampions();
-    Object.keys(songs);
-
-    const map = { };
-
-    let aux = 1;
-
-    for (var key of Object.keys(songs)){
-        map[`.item-${aux}`] = songs[key];
-        aux += 1;
-    }
-
-    const player = new Player(map)
 });
 
+
 const startChampions = async () => {
-    const api = "https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json"
+    const api = "https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json";
     const response = await fetch(api);
-    if (!response.ok){
-        throw new Error('Fallo en la lectura de la api')
+    if (!response.ok) {
+        throw new Error('Fallo en la lectura de la API');
     }
 
-    const data = await response.json()
-    const all_warriors = data.data
+    const data = await response.json();
+    const all_warriors = data.data;
 
     Object.keys(all_warriors).forEach(character => {
         let champion = new Champion(all_warriors[character]);
         pushChampion(champion);
     });
 
-    console.log(Champions)
-
+    console.log(Champions);
     showChampions();
 }
 
-function pushChampion(Champion) {
-    Champions.push(Champion);
+function pushChampion(champion) {
+    Champions.push(champion);
 }
 
 const showChampions = async () => {
     const ChampionsList = document.getElementById("champion_list");
-    for(var i = 0; i < Champions.length; i++) {
-        ChampionsList.innerHTML +=    `<div class="card">
-                                    <img class="front" src="${Champions[i].sprite}">
-                                    <br>
-                                    <p class="name">${Champions[i].name}</p>
-                                    <p class="title">${Champions[i].title}</p>
-                                    <br>
-                                    <div class="attributes"> 
-                                    <p>${Champions[i].attack} attack</p>
-                                    <p>${Champions[i].magic} magic</p>
-                                    <p>${Champions[i].defense} defense</p>
-                                    <p>${Champions[i].difficulty} difficulty</p> 
-                                    </div>
-                                    <div class="tags">
-                                        ${Champions[i].tags}
-                                    </div>
-                                    <div class="partype">
-                                        ${Champions[i].partype}                        
-                                    </div>
-                                </div>`
-    }
+
+    ChampionsList.innerHTML = '';
+
+    Champions.forEach((champion, index) => {
+        const cardHTML = `
+            <div class="card">
+                <img class="front" src="${champion.sprite}">
+                <br>
+                <p class="name">${champion.name}</p>
+                <p class="title">${champion.title}</p>
+                <br>
+                <div class="attributes">
+                    <p>${champion.attack} attack</p>
+                    <p>${champion.magic} magic</p>
+                    <p>${champion.defense} defense</p>
+                    <p>${champion.difficulty} difficulty</p>
+                </div>
+                <div class="tags">
+                    ${champion.tags}
+                </div>
+                <div class="partype">
+                    ${champion.partype}
+                </div>
+            </div>`;
+
+        ChampionsList.innerHTML += cardHTML;
+    });
+
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add('animate');
+        }, index * 100);
+    });
 }
 
--- Javascript/songs.js
+++
@@ -1,9 +0,0 @@
-export default class Song {
-    constructor(songPath, audio) {
-        this.element = document.querySelector(songPath);
-        this.audio = new Audio(audio);
-    }
-}
-export function play_song(song){
-    song.audio.play();
-};
